7ca4df4ce487d046500bdc983ca1defd012a2361
Rita Casiello <rituzka@gmail.com>
2022-02-26 20:19:57 +0100

    screen 1 and list widget created

---
+++ lib/screens/listView1_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ListView 1'),
+        ),
+        body: ListView(
+          children: const [Text('Probando 1'), Text('Probando 2')],
+        ));
+  }
+}
