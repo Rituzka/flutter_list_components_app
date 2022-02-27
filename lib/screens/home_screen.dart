df6b5ec2be44d2f0c918651f4a30e3d7ce1eb72c
Rita Casiello <rituzka@gmail.com>
2022-02-27 20:27:36 +0100

    Designing the home screen with a list

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,11 +3,24 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  get center => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Components'),
+        backgroundColor: Colors.pink,
+        elevation: 0,
+      ),
+      body: ListView.separated(
+        itemCount: 20,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: const Icon(Icons.abc),
+          title: const Text('Titulo'),
+          onTap: () {},
+        ),
+        separatorBuilder: (_, __) => const Divider(),
       ),
     );
   }
