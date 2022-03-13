fd9a2e40a179bf39b0a6f1c8ae3525d840c2b1c7
Rita Casiello <rituzka@gmail.com>
2022-03-13 16:44:43 +0100

    dark theme added in app_theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -6,4 +6,9 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       primaryColor: Colors.pink,
       appBarTheme: const AppBarTheme(color: primary, elevation: 0));
+
+  static final ThemeData darkTheme = ThemeData.dark().copyWith(
+      primaryColor: Colors.pink,
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      scaffoldBackgroundColor: Colors.black);
 }
