33bd4f1737a28dfaa4dce258dba968bf427536c6
Rita Casiello <rituzka@gmail.com>
2022-03-13 17:07:09 +0100

    Card screen appbar and listview

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_list_components_app/themes/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Card Widget"),
       ),
+      body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+                child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.phone_android, color: AppTheme.primary),
+                  title: Text("Title"),
+                )
+              ],
+            ))
+          ]),
     );
   }
 }
