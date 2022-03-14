5f9a645ba3f1fa36cff54fe454abb0de5c70809b
Rita Casiello <rituzka@gmail.com>
2022-03-14 20:15:30 +0100

    image widget in Card Screen, new custom widget class

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -11,8 +11,13 @@ class CardScreen extends StatelessWidget {
         title: const Text("Card Widget"),
       ),
       body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          children: const [CustomCardType1()]),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: const [
+          CustomCardType1(),
+          SizedBox(height: 10),
+          CustomCardType2()
+        ],
+      ),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'package:flutter_list_components_app/widgets/custom_card_type1.dart';
+export 'package:flutter_list_components_app/widgets/custom_card_type2.dart';
