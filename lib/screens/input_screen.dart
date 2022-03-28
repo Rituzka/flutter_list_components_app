6ae3fc6af8cb67ce39d701011a53877405e1b28f
Rita Casiello <rituzka@gmail.com>
2022-03-28 21:47:29 +0200

    wrapped in a form widget, new bottom to save and keyboard hide

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -7,6 +7,16 @@ class InputScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name': 'Rita',
+      'last_name': 'Casiello',
+      'email': 'rita@rita.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Inputs & Forms'),
@@ -14,26 +24,46 @@ class InputScreen extends StatelessWidget {
         body: SingleChildScrollView(
           child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: Column(
-                children: const [
-                  CustominputField(
-                      labelText: 'Nombre', hintText: 'Nombre del usuario'),
-                  SizedBox(height: 30),
-                  CustominputField(
-                      labelText: 'Apellido', hintText: 'Apellido del usuario'),
-                  SizedBox(height: 30),
-                  CustominputField(
-                      labelText: 'Email',
-                      hintText: 'Email del usuario',
-                      keyboard: TextInputType.emailAddress),
-                  SizedBox(height: 30),
-                  CustominputField(
-                    labelText: 'Contraseña',
-                    hintText: 'Contraseña del usuario',
-                    obscure: true,
-                  ),
-                  SizedBox(height: 30),
-                ],
+              child: Form(
+                key: myFormKey,
+                child: Column(
+                  children: [
+                    const CustominputField(
+                        labelText: 'Nombre', hintText: 'Nombre del usuario'),
+                    const SizedBox(height: 30),
+                    const CustominputField(
+                        labelText: 'Apellido',
+                        hintText: 'Apellido del usuario'),
+                    const SizedBox(height: 30),
+                    const CustominputField(
+                        labelText: 'Email',
+                        hintText: 'Email del usuario',
+                        keyboard: TextInputType.emailAddress),
+                    const SizedBox(height: 30),
+                    const CustominputField(
+                      labelText: 'Contraseña',
+                      hintText: 'Contraseña del usuario',
+                      obscure: true,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      child: const SizedBox(
+                          width: double.infinity,
+                          child: Center(child: Text('Save'))),
+                      onPressed: () {
+                        //para esconder teclado
+                        FocusScope.of(context).requestFocus(FocusNode());
+
+                        if (!myFormKey.currentState!.validate()) {
+                          print('Not valid Form');
+                          return;
+                        }
+                        //TODO print form's
+                        print(formValues);
+                      },
+                    )
+                  ],
+                ),
               )),
         ));
   }
